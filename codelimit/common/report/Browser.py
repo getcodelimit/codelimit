5fd97a12f30eea859e869a2101df5e9cc474b326
Rob van der Leek <robvanderleek@gmail.com>
2023-01-10 11:02:25 +0000

    Show code snippet

--- codelimit/common/report/Browser.py
+++ codelimit/common/report/Browser.py
@@ -2,7 +2,8 @@ from InquirerPy import inquirer
 from InquirerPy.base import Choice
 
 from codelimit.common.report.Report import Report
-from codelimit.common.report.ReportUnit import format_report_unit
+from codelimit.common.report.ReportUnit import format_report_unit, ReportUnit
+from codelimit.common.source_utils import get_location_range
 
 
 class Browser:
@@ -13,5 +14,8 @@ class Browser:
         units = [Choice(value=unit, name=format_report_unit(unit)) for unit in
                  self.report.all_report_units_sorted_by_length_asc()]
         while True:
-            selected_unit = inquirer.select(message='Select unit', choices=units).execute()
-            print(selected_unit)
+            selected_unit: ReportUnit = inquirer.select(message='Select unit', choices=units).execute()
+            with open(selected_unit['file']) as file:
+                code = file.read()
+            snippet = get_location_range(code, selected_unit['measurement']['start'], selected_unit['measurement']['end'])
+            print(snippet)
