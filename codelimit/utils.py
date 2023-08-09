e0b6740943baa9947bf72dc8cd3d70087fa240aa
Rob van der Leek <robvanderleek@gmail.com>
2023-08-09 09:40:09 +0200

    Implemented basic pre-commit hook

---
+++ codelimit/utils.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+
+import requests
+import typer
+from rich.progress import Progress, SpinnerColumn, TextColumn
+
+
+def upload_report(path: Path, url: str) -> None:
+    data_template = (
+        '{{"repository": "getcodelimit/codelimit", "branch": "main", "report":{}}}'
+    )
+
+    if not path.exist():
+        raise FileNotFoundError(str(path))
+
+    with Progress(
+            SpinnerColumn(),
+            TextColumn("[progress.description]{task.description}"),
+            transient=True,
+    ) as progress:
+        progress.add_task(description=f"Uploading {path.name} to {url}", total=None)
+        result = requests.post(
+            url,
+            data=data_template.format(path.read_text()),
+            headers={"Content-Type": "application/json"},
+        )
+
+    if result.ok:
+        typer.secho("Uploaded", fg="green")
+    else:
+        typer.secho(f"Upload unsuccessful: {result.status_code}", fg="red")
+        raise typer.Exit(exit_code=1)
