74751f2c9ca62a545f01a1eaf69909517e85407c
Rob van der Leek <robvanderleek@gmail.com>
2023-08-13 16:54:26 +0200

    :recycle: Refactoring CLI interface

--- codelimit/__main__.py
+++ codelimit/__main__.py
@@ -1,89 +1,59 @@
 from pathlib import Path
-from typing import List
+from typing import List, Annotated
 
 import typer
 
-from codelimit.common.Scanner import scan, scan_file
-from codelimit.common.report.Report import Report
 from codelimit.common.report.ReportReader import ReportReader
-from codelimit.common.report.ReportUnit import ReportUnit, format_report_unit
 from codelimit.common.report.ReportWriter import ReportWriter
-from codelimit.languages.python.PythonLaguage import PythonLanguage
 from codelimit.tui.CodeLimitApp import CodeLimitApp
-from codelimit.utils import upload_report
-from rich import print
+from codelimit.utils import upload_report, generate_report, check_files
 
-cli = typer.Typer()
-pre_commit_hook = typer.Typer()
+cli = typer.Typer(no_args_is_help=True, add_completion=False)
 
 
-@cli.callback(invoke_without_command=True)
-def cli_callback(
-    path: Path,
-    report_path: Path = typer.Option(
-        None,
-        "--report",
-        "-r",
-        help="JSON report for a code base",
-    ),
-    upload: bool = typer.Option(False, "--upload", help="Upload a report"),
+@cli.command(help="Scan a codebase")
+def scan(
+    path: Annotated[
+        Path, typer.Argument(file_okay=False, exists=True, help="Codebase root")
+    ]
+):
+    report_path = path.joinpath("codelimit.json").resolve()
+    if not report_path.exists():
+        report = generate_report(path)
+        report_path.write_text(ReportWriter(report).to_json())
+    else:
+        report = ReportReader.from_json(report_path.read_text())
+    app = CodeLimitApp(report)
+    app.run()
+
+
+@cli.command(help="Check file(s)")
+def check(paths: List[Path]):
+    exit_code = check_files(paths)
+    raise typer.Exit(code=exit_code)
+
+
+@cli.command(help="Upload report to CodeLimit server", hidden=True)
+def upload(
+    report_path: Path = typer.Argument(help="JSON report for a code base"),
     url: str = typer.Option(
         "https://codelimit-web.vercel.app/api/upload",
         "--url",
         "-u",
         help="Upload JSON report to this URL.",
     ),
-) -> None:
+):
+    try:
+        upload_report(report_path, url)
+        raise typer.Exit(code=0)
+    except FileNotFoundError as error:
+        typer.secho(f"File not found: {error}", fg="red")
+    raise typer.Exit(code=1) from None
+
+
+@cli.callback()
+def main():
     """CodeLimit: Your refactoring alarm"""
-
-    report_path = report_path or path.joinpath("codelimit.json").resolve()
-
-    if upload:
-        try:
-            upload_report(report_path, url)
-            raise typer.Exit(code=0)
-        except FileNotFoundError as error:
-            typer.secho(f"File not found: {error}", fg="red")
-        raise typer.Exit(code=1) from None
-
-    if not report_path.exists():
-        codebase = scan(path)
-        codebase.aggregate()
-        report = Report(codebase)
-        report_path.write_text(ReportWriter(report).to_json())
-    else:
-        report = ReportReader.from_json(report_path.read_text())
-
-    app = CodeLimitApp(report)
-    app.run()
-
-
-@pre_commit_hook.callback(invoke_without_command=True)
-def pre_commit_hook_callback(paths: List[Path]):
-    exit_code = 0
-    language = PythonLanguage()
-    for path in paths:
-        measurements = scan_file(language, str(path))
-        medium_risk = sorted(
-            [m for m in measurements if 30 < m.value <= 60],
-            key=lambda measurement: measurement.value,
-            reverse=True,
-        )
-        high_risk = sorted(
-            [m for m in measurements if m.value > 60],
-            key=lambda measurement: measurement.value,
-            reverse=True,
-        )
-        if medium_risk:
-            print(f"🔔 {path}")
-            for m in medium_risk:
-                print(format_report_unit(ReportUnit(str(path), m)))
-        if high_risk:
-            print(f"🚨 {path}")
-            for m in high_risk:
-                print(format_report_unit(ReportUnit(str(path), m)))
-            exit_code = 1
-    raise typer.Exit(code=exit_code)
 
 
 if __name__ == "__main__":
--- codelimit/utils.py
+++ codelimit/utils.py
@@ -1,8 +1,47 @@
 from pathlib import Path
+from typing import List
 
 import requests  # type: ignore
 import typer
 from rich.progress import Progress, SpinnerColumn, TextColumn
+
+from codelimit.common.Scanner import scan, scan_file
+from codelimit.common.report.Report import Report
+from codelimit.common.report.ReportUnit import format_report_unit, ReportUnit
+from codelimit.languages.python.PythonLaguage import PythonLanguage
+
+
+def generate_report(path: Path) -> Report:
+    codebase = scan(path)
+    codebase.aggregate()
+    return Report(codebase)
+
+
+def check_files(paths: List[Path]):
+    exit_code = 0
+    language = PythonLanguage()
+    for path in paths:
+        measurements = scan_file(language, str(path))
+        medium_risk = sorted(
+            [m for m in measurements if 30 < m.value <= 60],
+            key=lambda measurement: measurement.value,
+            reverse=True,
+        )
+        high_risk = sorted(
+            [m for m in measurements if m.value > 60],
+            key=lambda measurement: measurement.value,
+            reverse=True,
+        )
+        if high_risk:
+            print(f"🚨 {path}")
+            for m in high_risk:
+                print(format_report_unit(ReportUnit(str(path), m)))
+            exit_code = 1
+        if medium_risk:
+            print(f"🔔 {path}")
+            for m in medium_risk:
+                print(format_report_unit(ReportUnit(str(path), m)))
+    return exit_code
 
 
 def upload_report(path: Path, url: str) -> None:
