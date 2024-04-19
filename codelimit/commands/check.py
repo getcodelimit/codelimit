2cedaee9e8f5b734f08fe41c4863802bd5de6746
Rob van der Leek <5324924+robvanderleek@users.noreply.github.com>
2024-04-19 10:49:43 +0200

    fix: Another fix check for lexer name in languages

--- codelimit/commands/check.py
+++ codelimit/commands/check.py
@@ -8,7 +8,7 @@ from codelimit.common.CheckResult import CheckResult
 from codelimit.common.Scanner import is_excluded, scan_file
 from codelimit.common.lexer_utils import lex
 from codelimit.common.utils import load_language_by_name
-from codelimit.languages import LanguageName
+from codelimit.languages import language_names
 
 
 def check_command(paths: list[Path], quiet: bool):
@@ -28,9 +28,9 @@ def check_command(paths: list[Path], quiet: bool):
                     check_file(abs_path, check_result)
     exit_code = 1 if check_result.unmaintainable > 0 else 0
     if (
-        not quiet
-        or check_result.hard_to_maintain > 0
-        or check_result.unmaintainable > 0
+            not quiet
+            or check_result.hard_to_maintain > 0
+            or check_result.unmaintainable > 0
     ):
         check_result.report()
     raise typer.Exit(code=exit_code)
@@ -38,14 +38,14 @@ def check_command(paths: list[Path], quiet: bool):
 
 def check_file(path: Path, check_result: CheckResult):
     lexer = get_lexer_for_filename(path)
-    language = lexer.__class__.name
-    if language in LanguageName:
+    lexer_name = lexer.__class__.name
+    if lexer_name in language_names:
         with open(path) as f:
             code = f.read()
         tokens = lex(lexer, code, False)
-        language = load_language_by_name(lexer.__class__.name)
-        if language:
-            measurements = scan_file(tokens, language)
+        lexer_name = load_language_by_name(lexer.__class__.name)
+        if lexer_name:
+            measurements = scan_file(tokens, lexer_name)
             risks = sorted(
                 [m for m in measurements if m.value > 30],
                 key=lambda measurement: measurement.value,
