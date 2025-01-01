2b8e02a07daffb9d24ea66f24b164736d8ed9391
Rob van der Leek <5324924+robvanderleek@users.noreply.github.com>
2025-01-01 23:06:27 +0100

    chore: 🚨 Fix ruff

--- tests/common/token_matching/predicates/test_And.py
+++ tests/common/token_matching/predicates/test_And.py
@@ -2,7 +2,7 @@ from codelimit.common.token_matching.predicate.And import And
 from codelimit.common.token_matching.predicate.Keyword import Keyword
 from codelimit.common.token_matching.predicate.Not import Not
 from codelimit.common.token_matching.predicate.Symbol import Symbol
-from tests.common.token_matching.predicates.utils import keyword_token, symbol_token, operator_token
+from tests.common.token_matching.predicates.utils import keyword_token, symbol_token
 
 
 def test_and():
